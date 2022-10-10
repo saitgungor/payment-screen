ded0e746f10a36e1fcb67a926ab16aae95d9365f
saitgungor <osmansaitgungor@gmail.com>
2022-10-10 13:38:24 +0300

    setted up the redux store

--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,9 +1,27 @@
+import { useSelector } from "react-redux";
+
 import classes from "./CreditCard.module.css";
 
 const CreditCard = () => {
+  const { numberOnCard } = useSelector((state) => state.number);
   return (
     <div className={classes.card_wrapper}>
-      <div className={classes.credit_card}></div>
+      <div className={classes.credit_card}>
+        {/* <span className={classes.logo}>
+          <img src="/images/mastercard-logo.png" />
+        </span> */}
+        <span className={classes.logo}>
+          <img src="/images/visa-logo.png" />
+        </span>
+        <span className={classes.card_chip}>
+          <img src="/images/card-chip.png" />
+        </span>
+        <span className={classes.name}>Your full name</span>
+        <span className={classes.date}>Exp. date</span>
+        <span className={classes.number}>
+          {numberOnCard || "#### #### #### ####"}
+        </span>
+      </div>
     </div>
   );
 };
